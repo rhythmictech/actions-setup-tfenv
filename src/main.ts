564d34ff5f52c28cd3b46964e750791c8f4dec5d
Steven Black <genseb7@gmail.com>
2022-06-26 12:17:58 -0400

    build 1

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as tc from '@actions/tool-cache'
+import fetch from 'node-fetch'
+
+const TFENV_REPO_OWNER = 'tfutils'
+const TFENV_REPO_NAME = 'tfenv'
+const TFENV_VERSION = 'latest'
+const RELEASE_URL = `https://api.github.com/repos/${TFENV_REPO_OWNER}/${TFENV_REPO_NAME}/releases/${TFENV_VERSION}`
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
+    const release_response = await fetch(RELEASE_URL)
+    const release_json = await release_response.json()
+    core.debug(JSON.stringify(release_json))
+    const tarball_url = release_json['tarball_url']
+
+    const tfenv_path = await tc.downloadTool(tarball_url)
+    const tfenv_extracted_folder = await tc.extractTar(tfenv_path)
+    core.addPath(tfenv_extracted_folder + '/bin')
+
+    core.debug(new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
