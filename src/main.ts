6f149834e74a4a70632ce77829d384074e4291a4
Steven Black <genseb7@gmail.com>
2022-06-26 13:12:00 -0400

    much better

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
 import * as core from '@actions/core'
-import * as tc from '@actions/tool-cache'
-import fetch from 'node-fetch'
+import * as exec from '@actions/exec'
+import fs from 'fs'
 
-const TFENV_REPO_OWNER = 'tfutils'
-const TFENV_REPO_NAME = 'tfenv'
-const TFENV_VERSION = 'latest'
-const RELEASE_URL = `https://api.github.com/repos/${TFENV_REPO_OWNER}/${TFENV_REPO_NAME}/releases/${TFENV_VERSION}`
+const INSTALL_PATH = `${process.env.HOME}/.tfenv`
 
 async function run(): Promise<void> {
   try {
     core.debug(new Date().toTimeString())
 
-    const release_response = await fetch(RELEASE_URL)
-    const release_json = await release_response.json()
-    core.debug(JSON.stringify(release_json))
-    const tarball_url = release_json['tarball_url']
+    if (!fs.existsSync(INSTALL_PATH)) {
+      await exec.exec('git', [
+        'clone',
+        'https://github.com/tfutils/tfenv.git',
+        INSTALL_PATH
+      ])
+    } else {
+      core.debug(`tfenv already exists at ${INSTALL_PATH}`)
+    }
 
-    const tfenv_path = await tc.downloadTool(tarball_url)
-    const tfenv_extracted_folder = await tc.extractTar(tfenv_path)
-    core.addPath(tfenv_extracted_folder + '/bin')
+    const tfenv_path = INSTALL_PATH + '/bin'
+
+    core.debug(`Adding to path: ${tfenv_path}`)
+
+    core.addPath(tfenv_path)
 
     core.debug(new Date().toTimeString())
   } catch (error) {
